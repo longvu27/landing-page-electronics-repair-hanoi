88dc3a15487057d341992029163e720d634b18ae
longvu <nguyendoanlongvu.mochi@gmail.com>
2025-12-09 11:11:29 +0700

    add gtg

--- app/layout.js
+++ app/layout.js
@@ -79,6 +79,16 @@ export default function RootLayout({ children }) {
             gtag('config', 'AW-17631756339');
           `}
         </Script>
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PGHV33VK');
+          `}
+        </Script>
+
       </head>
       <body className={roboto.variable}>
         <a href="https://www.facebook.com/anh.nguyen.173196" width={0} height={0} />
